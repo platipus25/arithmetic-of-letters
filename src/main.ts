cfea84df1f260c4f37160d244731749a7000b858
platipus25 <40722163+platipus25@users.noreply.github.com>
2023-06-26 00:49:14 -0700

    Make grammar left-associative

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,56 @@
+import './styles.css'
 import grammar from './arithmeticofletters.ohm-bundle'
+import source from './arithmeticofletters.ohm?raw'
+
 
 const semantics = grammar.createSemantics()
 
 semantics.addOperation('repr', {
-    ConcatExp(a, _, b) { return `concat(${a.repr()}, ${b.repr()})` },
-    PriExp(_1, e, _2) { return e.repr() },
-    AddExp(a, _, b) { return `add(${a.repr()}, ${b.repr()})` },
-    SubExp(a, _, b) { return `sub(${a.repr()}, ${b.repr()})` },
-    XorExp(a, _, b) { return `xor(${a.repr()}, ${b.repr()})` },
-    CharLiteral(_) {
+    Char_paren(_1, e, _2) { return e.repr() },
+    Expression_concat(a, _, b) { return `concat(${a.repr()}, ${b.repr()})` },
+    Expression_add(a, _, b) { return `add(${a.repr()}, ${b.repr()})` },
+    Expression_sub(a, _, b) { return `sub(${a.repr()}, ${b.repr()})` },
+    Expression_and(a, _, b) { return `and(${a.repr()}, ${b.repr()})` },
+    Expression_or(a, _, b) { return `or(${a.repr()}, ${b.repr()})` },
+    Expression_xor(a, _, b) { return `xor(${a.repr()}, ${b.repr()})` },
+    Char_literal(_) {
         return `'${this.sourceString}'`
     },
 })
 
-//let charRenderingCanvas = new OffscreenCanvas(1000, 1000)
+semantics.addOperation('pretty', {
+    Char_paren(_1, e, _2) { return `(${e.pretty()})` },
+    Expression_concat(a, _, b) { return `${a.pretty()} || ${b.pretty()}` },
+    Expression_add(a, _, b) { return `${a.pretty()} + ${b.pretty()}` },
+    Expression_sub(a, _, b) { return `${a.pretty()} - ${b.pretty()}` },
+    Expression_and(a, _, b) { return `${a.pretty()} & ${b.pretty()}` },
+    Expression_or(a, _, b) { return `${a.pretty()} | ${b.pretty()}` },
+    Expression_xor(a, _, b) { return `${a.pretty()} ^ ${b.pretty()}` },
+    Char_literal(_) {
+        return `${this.sourceString}`
+    },
+})
+
 let color = 0;
+let font = "100px roboto";
+
+function CompositeChars(a: ImageBitmap, b: ImageBitmap, compositeOperation: GlobalCompositeOperation) {
+    let width = Math.max(a.width, b.width);
+    let height = Math.max(a.height, b.height);
+    
+    const canvas = new OffscreenCanvas(width, height)
+    const ctx = canvas.getContext('2d')!
+    
+    ctx.drawImage(a, 0, 0) // destination
+    ctx.globalCompositeOperation = compositeOperation;
+    ctx.drawImage(b, 0, 0) // source
+    
+    return canvas.transferToImageBitmap()
+}
 
 semantics.addOperation<ImageBitmap>('bitmap', {
-    PriExp(_1, e, _2) { return e.bitmap() },
-    ConcatExp(first, _, second) {
+    Char_paren(_1, e, _2) { return e.bitmap() },
+    Expression_concat(first, _, second) {
         let a = first.bitmap()
         let b = second.bitmap()
 
@@ -32,57 +64,22 @@ semantics.addOperation<ImageBitmap>('bitmap', {
 
         return canvas.transferToImageBitmap()
     },
-    AddExp(first, _, second) { 
-        let a = first.bitmap()
-        let b = second.bitmap()
-        
-        let width = Math.max(a.width, b.width);
-        let height = Math.max(a.height, b.height);
-        
-        const canvas = new OffscreenCanvas(width, height)
-        const ctx = canvas.getContext('2d')!
-        
-        ctx.drawImage(a, 0, 0) // destination
-        ctx.globalCompositeOperation = "source-over";
-        ctx.drawImage(b, 0, 0) // source
-        
-        return canvas.transferToImageBitmap()
+    Expression_add(a, _, b) { 
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-over");
     },
-    SubExp(first, _, second) {
-        let a = first.bitmap()
-        let b = second.bitmap()
-
-        let width = Math.max(a.width, b.width);
-        let height = Math.max(a.height, b.height);
-        
-        const canvas = new OffscreenCanvas(width, height)
-        const ctx = canvas.getContext('2d')!
-
-        ctx.drawImage(a, 0, 0) // destination
-        ctx.globalCompositeOperation = "destination-out";
-        ctx.drawImage(b, 0, 0) // source
-        
-        return canvas.transferToImageBitmap()
+    Expression_sub(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "destination-out");
     },
-    XorExp(first, _, second) {
-        let a = first.bitmap()
-        let b = second.bitmap()
-
-        let width = Math.max(a.width, b.width);
-        let height = Math.max(a.height, b.height);
-        
-        const canvas = new OffscreenCanvas(width, height)
-        const ctx = canvas.getContext('2d')!
-
-        ctx.drawImage(a, 0, 0) // destination
-        ctx.globalCompositeOperation = "xor";
-        ctx.drawImage(b, 0, 0) // source
-        
-        return canvas.transferToImageBitmap()
+    Expression_and(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-in");
     },
-    CharLiteral(_) {
-        const font = "100px monospace";
-
+    Expression_or(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-over");
+    },
+    Expression_xor(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "xor");
+    },
+    Char_literal(_) {
         let canvas = new OffscreenCanvas(100, 100);
         let ctx = canvas.getContext('2d')! 
 
@@ -106,25 +103,26 @@ semantics.addOperation<ImageBitmap>('bitmap', {
     },
 })
 
-let result = grammar.match("(G - H) || (J ^ H) || (M + N) + (O + P)")
-let adapter = semantics(result)
-
-console.log("HI")
-console.log(result, result.succeeded(), adapter.repr())
-
 document.addEventListener('DOMContentLoaded', async () => {
     await update(
         document.getElementById("input")! as HTMLInputElement,
         document.getElementById("output")! as HTMLImageElement,
     )
+
+    document.getElementById('source')!.innerText = source//grammar.source.sourceString
 })
 
-async function render(text: string) : Promise<Blob> {
-    let result = grammar.match(text)
+async function update(input: HTMLInputElement, output: HTMLImageElement) {
+    let result = grammar.match(input.value)
     if (result.failed()) {
-        throw SyntaxError(`Failed to parse: ${result.message}`)
+        console.warn(`Failed to parse: ${result.message}`)
+        return
     }
-    let adapter = semantics(result)
+    let adapter = semantics(result)  
+
+    let pretty = adapter.pretty()
+    //input.value = pretty;
+    
     let bitmap = adapter.bitmap()
     color = 0;
 
@@ -132,14 +130,8 @@ async function render(text: string) : Promise<Blob> {
     let ctx = canvas.getContext('2d')!
     ctx.drawImage(bitmap, 0, 0)
     const blob = await canvas.convertToBlob()
-    return blob
-}
-
-async function update(input: HTMLInputElement, output: HTMLImageElement) {
-    await render(input.value).then((blob) => {
-        const url = URL.createObjectURL(blob);
-        output.src = url
-    }).catch((err) => console.error(err))
+    const url = URL.createObjectURL(blob);
+    output.src = url
 }
 
 document.getElementById("input")?.addEventListener('input', async (event) => {
---
+++ src/parser.ts
@@ -0,0 +1,40 @@
+//import * as ohm from 'ohm-js';
+/*
+enum Token {
+  OpenParenthesis,
+  CloseParenthesis,
+  Character,
+  Operator,
+}
+
+
+enum OperatorTypes {
+  Add,
+  Subtract,
+  Concat,
+}*/
+
+/*class Operator {
+  static
+
+
+  static isOperator(s: string) {
+    return
+  }
+}
+
+class Parser {
+  constructor(input: string) {
+
+  }
+
+  parse() {
+    for (/)
+  }
+}
+*/
+
+
+/*const example = `
+P + \ || @ - O || (> + |) ^ 7 + G + 0 ^ (- + ^)
+`*/
