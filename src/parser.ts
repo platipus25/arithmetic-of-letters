e88180507d781c508adab76c6fce582cd419d751
platipus25 <40722163+platipus25@users.noreply.github.com>
2023-06-29 01:15:47 -0700

    Move grammar semantics into separate file

--- src/parser.ts
+++ src/parser.ts
@@ -1,40 +1,107 @@
-//import * as ohm from 'ohm-js';
-/*
-enum Token {
-  OpenParenthesis,
-  CloseParenthesis,
-  Character,
-  Operator,
+import grammar from './arithmeticofletters.ohm-bundle'
+
+const semantics = grammar.createSemantics()
+
+semantics.addAttribute('repr', {
+    Char_paren(_1, e, _2) { return e.repr },
+    Expr_concat(a, _, b) { return `concat(${a.repr}, ${b.repr})` },
+    Expr_add(a, _, b) { return `add(${a.repr}, ${b.repr})` },
+    Expr_sub(a, _, b) { return `sub(${a.repr}, ${b.repr})` },
+    Expr_and(a, _, b) { return `and(${a.repr}, ${b.repr})` },
+    Expr_or(a, _, b) { return `or(${a.repr}, ${b.repr})` },
+    Expr_xor(a, _, b) { return `xor(${a.repr}, ${b.repr})` },
+    Char_literal(_) {
+        return `'${this.sourceString}'`
+    },
+})
+
+semantics.addAttribute('pretty', {
+    Char_paren(_1, e, _2) { return `(${e.pretty})` },
+    Expr_concat(a, _, b) { return `${a.pretty} || ${b.pretty}` },
+    Expr_add(a, _, b) { return `${a.pretty} + ${b.pretty}` },
+    Expr_sub(a, _, b) { return `${a.pretty} - ${b.pretty}` },
+    Expr_and(a, _, b) { return `${a.pretty} & ${b.pretty}` },
+    Expr_or(a, _, b) { return `${a.pretty} | ${b.pretty}` },
+    Expr_xor(a, _, b) { return `${a.pretty} ^ ${b.pretty}` },
+    Char_literal(_) {
+        return `${this.sourceString}`
+    },
+})
+
+let hue: number = 0;
+let font = "100px roboto";
+
+function CompositeChars(a: ImageBitmap, b: ImageBitmap, compositeOperation: GlobalCompositeOperation) {
+    let width = Math.max(a.width, b.width);
+    let height = Math.max(a.height, b.height);
+    
+    const canvas = new OffscreenCanvas(width, height)
+    const ctx = canvas.getContext('2d')!
+    
+    ctx.drawImage(a, 0, 0) // destination
+    ctx.globalCompositeOperation = compositeOperation;
+    ctx.drawImage(b, 0, 0) // source
+    
+    return canvas.transferToImageBitmap()
 }
 
+semantics.addOperation<ImageBitmap>('bitmap', {
+    Char_paren(_1, e, _2) { return e.bitmap() },
+    Expr_concat(first, _, second) {
+        let a = first.bitmap()
+        let b = second.bitmap()
+        hue = 0;
 
-enum OperatorTypes {
-  Add,
-  Subtract,
-  Concat,
-}*/
+        let width = a.width + b.width;
+        let height = Math.max(a.height, b.height);
 
-/*class Operator {
-  static
+        const canvas = new OffscreenCanvas(width, height)
+        const ctx = canvas.getContext('2d')!
+        ctx.drawImage(a, 0, 0)
+        ctx.drawImage(b, a.width, 0)
 
+        return canvas.transferToImageBitmap()
+    },
+    Expr_add(a, _, b) { 
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-over");
+    },
+    Expr_sub(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "destination-out");
+    },
+    Expr_and(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-in");
+    },
+    Expr_or(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "source-over");
+    },
+    Expr_xor(a, _, b) {
+        return CompositeChars(a.bitmap(), b.bitmap(), "xor");
+    },
+    Char_literal(_) {
+        let canvas = new OffscreenCanvas(100, 100);
+        let ctx = canvas.getContext('2d')! 
 
-  static isOperator(s: string) {
-    return
-  }
-}
+        ctx.font = font;
+        const textMetrics = ctx.measureText(this.sourceString);
 
-class Parser {
-  constructor(input: string) {
+        let width = textMetrics.width;
+        let height = textMetrics.fontBoundingBoxAscent + textMetrics.fontBoundingBoxDescent;
 
-  }
+        canvas = new OffscreenCanvas(width, height);
+        ctx = canvas.getContext('2d')! 
+    
+        ctx.font = font;
+        ctx.fillStyle = `hsl(${hue % 360}, 100%, 60%)`
+        hue += 80.0
 
-  parse() {
-    for (/)
-  }
-}
-*/
+        ctx.fillText(this.sourceString, 0, textMetrics.fontBoundingBoxAscent)
+    
+        
+        return canvas.transferToImageBitmap()
+    },
+})
 
-
-/*const example = `
-P + \ || @ - O || (> + |) ^ 7 + G + 0 ^ (- + ^)
-`*/
+export {
+  grammar,
+  semantics 
+};
