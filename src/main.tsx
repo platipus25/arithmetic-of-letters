dbd5b3465423f69822bab78589ec9a1446d4f75c
platipus25 <40722163+platipus25@users.noreply.github.com>
2023-07-04 12:03:33 -0700

    Lots more UI things
    (and auto prettifying)

--- src/main.tsx
+++ src/main.tsx
@@ -42,11 +42,11 @@ function ExpressionRenderer(props: {
 
 function Input(props: any) {
   return (
-    <div class="flex p-0 border-0 border-b-4 text-gray-900 border-gray-400  focus-within:border-indigo-400 focus:ring-inset sm:leading-6">
+    <div class="flex p-0 border-0 border-b-4 text-gray-900 border-indigo-500 dark:text-gray-300 focus-within:border-indigo-400 focus:ring-inset sm:leading-6">
       <input
         {...props}
         type="text"
-        class="block border-0 my-0 w-full focus:ring-0 mx-2 px-1 py-0.5 text-lg border-t border-t-gray-300 justify-center placeholder:text-gray-400 focus:border-t-gray-300"
+        class="block dark:bg-inherit border-0 my-0 w-full focus:ring-0 mx-2 px-1 py-0.5 text-lg border-t dark:border-neutral-600 dark:focus:border-neutral-600 focus:border-gray-300  border-gray-300 justify-center placeholder:text-gray-400"
       />
     </div>
   );
@@ -58,6 +58,14 @@ const App = () => {
 
   const match = createMemo(() => grammar.match(text()));
 
+  const prettyText = () => {
+    if (match().failed()) {
+      return text();
+    }
+    const adapter = semantics(match());
+    setText(adapter.pretty);
+  };
+
   createEffect(() => {
     if (match().succeeded()) {
       return;
@@ -67,58 +75,77 @@ const App = () => {
   });
 
   return (
-    <div class="grid relative">
-      <div id="headerbox" class="left-0 right-0 top-0 p-2 bg-gray-800">
-        <h1 class="text-2xl font-display text-gray-100">
+    <div class="grid relative auto-cols-auto auto-rows-max md:grid-flow-col md:grid-cols-3 dark:bg-neutral-700">
+      <div
+        id="headerbox"
+        class="col-span-full left-0 right-0 top-0 p-2 bg-gray-300 dark:bg-neutral-900"
+      >
+        <h1 class="text-2xl font-display text-black dark:text-gray-300">
           Arithmetic of Letters
         </h1>
       </div>
 
-      <div id="displaybox" class="grid notsticky top-0 justify-center">
-        <ExpressionRenderer
-          fontFamily="roboto"
-          fontSize={`${fontSize()}px`}
-          match={match()}
-          class="h-60 justify-self-center hover:drop-shadow"
-        />
-        <Show when={match().failed()}>
-          <pre class="text-rose-500 mx-2">{match().message}</pre>
-        </Show>
+      <div
+        id="displaybox"
+        class="grid md:col-span-2 md:col-start-2 grid-flow-row md:grid-rows-3 grid-cols-1 place-items-center"
+      >
+        <div id="expressionbox" class="overflow-scroll">
+          <ExpressionRenderer
+            fontFamily="roboto"
+            fontSize={`${fontSize()}px`}
+            match={match()}
+            class="h-60 justify-self-center hover:drop-shadow m-auto min-w-min"
+          />
+        </div>
+        <pre class="text-rose-500 mx-2 order-3 md:order-2">
+          {match().message}
+        </pre>
+
+        <div
+          id="inputbox"
+          class="sticky p-4 justify-self-stretch order-2 md:order-3"
+        >
+          <Input
+            oninput={(e: InputEvent & { currentTarget: HTMLInputElement }) =>
+              setText(e.currentTarget.value)
+            }
+            onfocusout={() => prettyText()}
+            value={text()}
+            name="expression"
+            placeholder="expression"
+          />
+        </div>
       </div>
 
-      <div id="inputbox" class="sticky p-2">
-        <Input
-          oninput={(e: InputEvent & { currentTarget: HTMLInputElement }) =>
-            setText(e.currentTarget.value)
-          }
-          value={text()}
-          name="expression"
-          placeholder="expression"
-        />
-      </div>
+      <div
+        id="sidebar"
+        class="md:bg-gray-200 md:dark:bg-neutral-800 col-start-1 md:row-start-2 row-span-1 md:drop-shadow-md dark:text-gray-200"
+      >
+        <div id="controlsbox" class="grid px-4 py-2">
+          <label for="fontsize" class="m-1">
+            Font Size
+          </label>
+          <input
+            name="fontsize"
+            type="range"
+            min="2"
+            max="1000"
+            oninput={(e) => setFontSize(parseInt(e.currentTarget.value))}
+            value={fontSize()}
+          />
+          <label for="fontfamily" class="m-1">
+            Font Family (Coming Soon)
+          </label>
+          <label for="colorstrategy" class="m-1">
+            Color Palette (Coming Soon)
+          </label>
+        </div>
 
-      <div id="controlsbox" class="grid px-4 py-2">
-        <label for="fontsize" class="m-1">
-          Font Size
-        </label>
-        <input
-          name="fontsize"
-          type="range"
-          min="2"
-          max="1000"
-          oninput={(e) => setFontSize(parseInt(e.currentTarget.value))}
-          value={fontSize()}
-        />
-        <label for="fontfamily" class="m-1">
-          Font Family (Coming Soon)
-        </label>
-        <label for="colorstrategy" class="m-1">
-          Color Palette (Coming Soon)
-        </label>
-      </div>
-
-      <div id="infobox" class="px-4 py-2">
-        <pre>{`
+        <div
+          id="infobox"
+          class="flex flex-col gap-2 px-4 py-2 overflow-y-scroll"
+        >
+          <pre>{`
 || concat
 +  add
 -  subtract
@@ -128,10 +155,15 @@ const App = () => {
 () parentheses
 any unicode character
 `}</pre>
-        <pre>{(grammar as any).source.sourceString}</pre>
+          <pre class="overflow-x-scroll">
+            {(grammar as any).source.sourceString}
+          </pre>
+        </div>
       </div>
 
-      <div id="footerbox" class="pb-14"></div>
+      {/*<div id="footerbox" class="grid h-14 bg-gray-300 md:col-span-2 self-end place-content-center">
+        🫨
+        </div>*/}
     </div>
   );
 };
